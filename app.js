ad09403ce23b3649525406507edc4ebd3805f634
AliMahmoudi <alimahmoudi21a@gmail.com>
2024-09-26 22:56:25 +0330

    selectElements

---
+++ app.js
@@ -0,0 +1,8 @@
+const searchBox_input = document.querySelector(".searchBox_input");
+const search_png = document.querySelector(".search_png");
+const mainPicture = document.querySelector(".mainPicture");
+const title__city = document.querySelector(".title__city");
+const title__temp = document.querySelector(".title__temp");
+const status = document.querySelector(".status");
+const humidity = document.querySelector(".humidity__info h5");
+const wind = document.querySelector(".wind__info h5");
